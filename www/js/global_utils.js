3333d221af5a646ad3e96cf70d73de29fb48182d
Peter Earle <PEarle@farnell.com>
2015-11-30 15:38:44 +0000

    Dummy files to test navigation

--- www/js/global_utils.js
+++ www/js/global_utils.js
@@ -6,13 +6,9 @@
  */
 function buildHeaderBar(title, returnFunction) {
 
-	var style = "width:320px; background:" + appStyle.barColour + "; height:" + appStyle.headerHeight + 'px;';
-	var html = '<div style="' + style + '">';
-	
-	// Back option returns to supplied function
-	html = html + '<span style="color:#FFFFFF; height:30px;  display:table-cell; vertical-align:middle; padding-left:10px" onclick="' + returnFunction + ';">Back</span>';
-	
-	html = html + '<span style="color:#FFFFFF; height:30px;  display:table-cell; vertical-align:middle; padding-left:20px" font-Size:' + appStyle.headerFont.fontSize + 'px; font-Weight:' + appStyle.headerFont.fontWeight + '">' + title + '</span>';
+	//var style = "width:320px; background:" + appStyle.barColour + "; height:" + appStyle.headerHeight + 'px;';
+	var html = '<div class="header_bar">';
+	html = html + '<span>' + title + '</span>';
 	html = html + "</div>";
 	
 	return html;
